9b725cc431fc1f3bb7a3b0fc54c15ba297d32fdb
FlorMarleny <flor.rodriguez@tecsup.edu.pe>
2024-04-04 01:09:17 -0500

    pantalla principal 80%

--- sistema_vehicular/settings.py
+++ sistema_vehicular/settings.py
@@ -97,7 +97,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/img/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
